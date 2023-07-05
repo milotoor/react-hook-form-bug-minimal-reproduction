6a32bc43786e75f2db86609b680e62a7059f8e2e
Milo Toor <milo@synop.ai>
2023-07-04 22:30:24 -0700

    Add README, refine example

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,58 @@
-import { useForm } from 'react-hook-form'
-import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-type Schema = yup.InferType<typeof schema>
-type ComponentProps = {
-  defaultValues?: Schema
-}
+// Types
+type ComponentProps<Schema extends yup.ISchema<any>> = {
+  defaultValues?: yup.InferType<Schema>;
+};
 
-const schema = yup
-  .object({
-    required: yup.number().required(),
-    optional: yup.number().optional(),
-  })
-  .required()
+// Schema definitions
+const schemaRequiredOnly = yup.object({ required: yup.number().required() });
+const schemaOptionalOnly = yup.object({ optional: yup.number().optional() });
+const schemaBoth = yup.object({
+  required: yup.number().required(),
+  optional: yup.number().optional(),
+});
 
-const App = ({ defaultValues }: ComponentProps) => {
+// Components
+const SchemaRequiredOnly = ({
+  defaultValues,
+}: ComponentProps<typeof schemaRequiredOnly>) => {
   useForm({
     defaultValues,
-    mode: 'all',
-    resolver: yupResolver(schema),
-  })
+    resolver: yupResolver(schemaRequiredOnly),
+  });
 
-  return null
+  return null;
+};
+
+const SchemaOptionalOnly = ({
+  defaultValues,
+}: ComponentProps<typeof schemaOptionalOnly>) => {
+  useForm({
+    defaultValues,
+    resolver: yupResolver(schemaOptionalOnly), // <-- error
+  });
+
+  return null;
+};
+
+const SchemaBoth = ({ defaultValues }: ComponentProps<typeof schemaBoth>) => {
+  useForm({
+    defaultValues,
+    resolver: yupResolver(schemaBoth), // <-- error
+  });
+
+  return null;
+};
+
+export default function App() {
+  return (
+    <>
+      <SchemaBoth />
+      <SchemaRequiredOnly />
+      <SchemaOptionalOnly />
+    </>
+  );
 }
-
-export default App
